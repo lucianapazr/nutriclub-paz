d4f851a88ae13ac844ee1e354a5fe1c76aa7e9fd
-Luciana Paz <pazr.luciana@gmail.com>
2021-09-22 00:00:23 -0300

    incorporacion de bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <>
-    
+      <NavBar />
+      <h2>Las ofertas de la semana</h2>
     </>
   );
 }
