a0b8ca719de842a22e637b49cb68b56fe94c6745
-Luciana Paz <pazr.luciana@gmail.com>
2021-10-08 01:55:26 -0300

    trabajo en detallar el routing dentro del nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import {Navbar,Container,Nav,NavDropdown} from 'react-bootstrap';
 import logo from './img/logo_grande.png';
@@ -8,28 +9,28 @@ import CartWidget from './CartWidget';
 const NavBar = () => {
     return (
         <Navbar className="navbar navbar-expand-lg navbar-light navbar-light nav-bg-opacity">
-  <Container>
-    <Navbar.Brand href="#home" className="alignLogo">
-        <img src={logo} alt="Nutriclub" width="40%"></img>
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="" className="alignNavbar">Sobre mi</Nav.Link>
-        <NavDropdown title="Recetas" id="basic-nav-dropdown" className="alignNavbar">
-          <NavDropdown.Item href="#action/3.1">Ensalada de pollo</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Yogur mix</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Pastas</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Omelette</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Helado casero</NavDropdown.Item>
-        </NavDropdown>
-        <Nav.Link href="" className="alignNavbar">Tienda</Nav.Link>
-        <Nav.Link href="" className="alignNavbar">Contacto</Nav.Link>
-      </Nav>
-      <CartWidget number={1}/>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+          <Container>
+            <Navbar.Brand href="#home" className="alignLogo"><img src={logo} alt="Nutriclub" width="40%"></img></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Link to="/">Inicio</Link>
+                <Link to="/" className="alignNavbar">Sobre mi</Link>
+                <NavDropdown title="Tienda" id="basic-nav-dropdown" className="alignNavbar">
+                  <NavDropdown.Item href="#action/3.1">Basta de dietas</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">100 Ideas</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Recetas saludables</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.4">Guia semanal</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.5">Fiestas: Salado</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.6">Fiestas: Dulce</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="" className="alignNavbar">Recetas</Nav.Link>
+                <Nav.Link href="" className="alignNavbar">Contacto</Nav.Link>
+              </Nav>
+              <CartWidget number={1}/>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
     )
 }
 
