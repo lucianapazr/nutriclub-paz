ce4639260d69faddcfb3b833adff376bf34d3264
-Luciana Paz <pazr.luciana@gmail.com>
2021-10-08 01:42:54 -0300

    detalle del routing de cada item

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,20 +1,21 @@
 import React from 'react'
-import ItemCount from './ItemCount'
+import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
 
 const Item = ({product}) =>{
 
     return (
         <div>
-            <Card className="text-center my-2">
-                <Card.Header>{product.title} - {product.author}</Card.Header>
-                <Card.Body>
-                    <Card.Img className="w-25" variant="top" src={product.pictureUrl} alt={product.title} />
-                    <Card.Text>${product.cost}</Card.Text>
-                    <ItemCount stock={product.stock} initial={0}/>
-                </Card.Body>
-                <Card.Footer className="text-muted">Stock: {product.stock}</Card.Footer>
-            </Card>           
+            <Link to={`/item/${product.id}`}>
+                <Card className="text-center my-2">
+                    <Card.Header>{product.title} - {product.author}</Card.Header>
+                    <Card.Body>
+                        <Card.Img className="w-25" variant="top" src={product.pictureUrl} alt={product.title} />
+                        <Card.Text>${product.cost}</Card.Text>
+                    </Card.Body>
+                    <Card.Footer className="text-muted">Stock: {product.stock}</Card.Footer>
+                </Card>
+            </Link>         
         </div>
     )
 }
