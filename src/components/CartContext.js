76a84554720406052d330566e8fcfaf12ddad650
-Luciana Paz <pazr.luciana@gmail.com>
2021-10-19 12:18:04 -0300

    error 19.10

--- src/components/CartContext.js
+++ src/components/CartContext.js
@@ -1,18 +1,18 @@
 import {createContext, useState} from "react";
 export const CartContext = createContext()
-//const {Provider} = CartContext
+const {Provider} = CartContext
 
-export const ProviderCustomizado = ({children}) => {
+const ProviderCustomizado = ({children}) => {
 
     const [carrito, setCarrito] = useState([]);
-   // const [contador, setContador] = useState(0);
+    const [contador, setContador] = useState(0);
 
-    //const valorDelContexto = {
-      //  carrito,
-        //setCarrito,
-        //contador,
-        //setContador
-    //}
+    const valorDelContexto = {
+      carrito,
+        setCarrito,
+        contador,
+        setContador
+    }
 
     return(
         <CartContext.Provider value={{carrito, setCarrito}}>
@@ -21,5 +21,4 @@ export const ProviderCustomizado = ({children}) => {
     )
 }
 
-
-export default CartContext
+export default ProviderCustomizado
